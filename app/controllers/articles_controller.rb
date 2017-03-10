de80e0db2f0252632792476c2c4c696bcbfd65e4
mikey neilson <mikeyneilson35@googlemail.com>
2017-03-10 13:47:59 +0000

    Added Articles controller and model, created form and create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,19 @@
 class ArticlesController < ApplicationController
 
-  def index
-    @articles = Article.all
-  end
-
-  def show
-    @article = Article.find(params[:id])
-  end
-
   def new
-    @article = Article.new
+    
   end
-  
-  def create
-    @article = Article.new(article_params)
+
+
+def create
+  @article = Article.new(article_params)
  
-  if @article.save
-    redirect_to @article
-  else 
-    render 'new'
-  end
+  @article.save
+  redirect_to @article
+end
+
+def show
+  
 end
  
 private
